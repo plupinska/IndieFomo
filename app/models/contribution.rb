112188c3921985eeaf1cffbdcba1b147f12dfe3b
patrycja lupinska <patrycjalupinska@gmail.com>
2017-05-18 16:05:38 -0400

    Update amount validation

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,5 +1,5 @@
 class Contribution < ActiveRecord::Base
-   validates_numericality_of :amount, {greater_than_or_equal_to: 0, allow_nil: false, message: "Please enter an amount greater than zero!"}
+   validates_numericality_of :amount, {greater_than_or_equal_to: 1, allow_nil: false, message: "Please enter an amount greater than zero!"}
 
   belongs_to :user
   belongs_to :campaign
