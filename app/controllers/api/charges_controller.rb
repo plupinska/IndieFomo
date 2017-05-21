3991e7d31eaa641a171d81ad717fffd3c87fd488
patrycja lupinska <patrycjalupinska@gmail.com>
2017-05-21 19:53:23 -0400

    remove stripe charges

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -1,28 +1,28 @@
-class Api::ChargesController < ApplicationController
-
-  def new
-  end
-
-  def create
-    #  default for now. Stripe ammounts must be in cents
-     
-    @amount = params[:amount]
-
-    customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-    :customer => customer.id,
-    :amount => @amount,
-    :description => 'Rails Stripe customer',
-    :currency => 'usd'
-    )
-
-  rescue = Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
-
-end
+# class Api::ChargesController < ApplicationController
+#
+#   def new
+#   end
+#
+#   def create
+#     #  default for now. Stripe ammounts must be in cents
+#
+#     @amount = params[:amount]
+#
+#     customer = Stripe::Customer.create(
+#     :email => params[:stripeEmail],
+#     :source => params[:stripeToken]
+#     )
+#
+#     charge = Stripe::Charge.create(
+#     :customer => customer.id,
+#     :amount => @amount,
+#     :description => 'Rails Stripe customer',
+#     :currency => 'usd'
+#     )
+#
+#   rescue = Stripe::CardError => e
+#     flash[:error] = e.message
+#     redirect_to new_charge_path
+#   end
+#
+# end
